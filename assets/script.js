c2dc4610c56800af820764570064025d68cb2391
jayjscotto <jayjscotto@gmail.com>
2019-09-21 14:44:31 -0400

    updated final questions and answers, fixed logic, still need to implement timer

--- assets/script.js
+++ assets/script.js
@@ -38,7 +38,6 @@
         //Give user results:
             //Questions answered Correctly
             //Questions answered incorrectly
-            //total time elapsed during the questions
 
         //Give option to reset/restart game
         //Give option to check out the code on github
@@ -51,49 +50,47 @@
 
     const csTrivia = {
         //variable to access index of question, choices and answer...
-        counter: 0,
+        counter: 1,
         score: 0,
         incorrectAnswers: 0,
-        totalTime: '',
 
         //questions: object property for each question
         questions: {
             question1: 'What year was the first computer invented?',
             question2: 'Which company first developed the Java programming language?',
-            question3: 'What was the original name for Javascript?',
+            question3: "What was the original name for Javascript on it's first release?",
             question4: 'What year was Microsoft Founded?',
-            question5: 'What does the acronym W3C represent?',
+            question5: 'A means of encoding text in which each symbol is represented by 16 bits?',
             question6: 'How much memory could the first floppy disc hold?',
             question7: 'How many bytes are in 2 Petabytes?',
-            question8: 'Intel or AMD?',
+            question8: 'A digital circuit capable of holding a single digit?',
             question9: 'What common element is used in the manufacture of computer chips?',
             question10: 'Does the console display every answer to these questions?',
-
         },
 
         //choices: possible answers for each question
         choices: {
-            choices1: ['Hello World','Please Help Me','Computers Are Great','Helllllllooo'],
-            choices2: ['A','B','C','D'],
-            choices3: ['A','B','C','D'],
-            choices4: ['A','B','C','D'],
-            choices5: ['A','B','C','D'],
-            choices6: ['A','B','C','D'],
-            choices7: ['A','B','C','D'],
-            choices8: ['A','B','C','D'],
-            choices9: ['A','B','C','D'],
+            choices1: ['1967','1946','1997','1985'],
+            choices2: ['Apple','Microsoft','Sun Microsystems','Macintosh'],
+            choices3: ['Mocha','LiveScript','ECMAScript','Javascript'],
+            choices4: ['1986','2001','1992','1975'],
+            choices5: ['ISO','ASCII','Unicode','UTF'],
+            choices6: ['80KB','90MB','2MB','1GB'],
+            choices7: ['300000','10000000000000000','400000000','10247681290'],
+            choices8: ['Letter','Bit','Flip Flop','Key'],
+            choices9: ['Graphite','Lead','Silicon','Aluminum'],
             choices10: ['It might', "I've been looking the whole time", 'No!', 'Probably..'],
         },
 
         //answers: actual single answers for each question
         answers: {
-            answer1: 'Hello World',
-            answer2: 'B',
-            answer3: 'C',
-            answer4: 'D',
+            answer1: '1946',
+            answer2: 'Sun Microsystems',
+            answer3: 'LiveScript',
+            answer4: '1975',
             answer5: 'C',
-            answer6: 'B',
-            answer7: 'A',
+            answer6: '80KB',
+            answer7: '10000000000000000',
             answer8: 'A',
             answer9: 'Silicon',
             answer10: "I've been looking the whole time",
@@ -103,17 +100,16 @@
         generateQuestion() {
             questionDiv.empty();
             questionDiv.append(Object.values(this.questions)[this.counter]);
-            this.counter++;
         },
 
         //adds buttons with corresponding possible answers to the page
         generateChoices() {
             let choicesArr = Object.values(this.choices)[this.counter];
-            console.log(choicesArr);
             for (let i = 0; i < 4; i++) {
                 let choiceButton = '<div class="col text-left ml-3"> <a class="btn btn-primary answer-choice btn-lg mx-auto my-3" href="#" role="button">' + choicesArr[i] + '</a> </div>';
                 answerDiv.append(choiceButton);
             }
+            this.counter++;
         },
 
         //reset to set score/incorrect #s back to 0 and empty divs
@@ -126,7 +122,6 @@
         }
     }
 
-    
     $('#play-game').on('click', function() {
         csTrivia.generateQuestion();
         introContainer.hide();
@@ -137,19 +132,24 @@
         //if button clicked contains the right answer
         if ($(this).html() === Object.values(csTrivia.answers)[csTrivia.counter]) {
             csTrivia.score++;
-            console.log(csTrivia.score);
+            console.log('score: ' + csTrivia.score);
+            answerDiv.empty();
             csTrivia.generateQuestion();
+            csTrivia.generateChoices();
         } else if (timer = 0) {
             //timer runs out
-            csTrivia.generateQuestion();
-        } else {
+            answerDiv.empty();
             csTrivia.incorrectAnswers++;
             csTrivia.generateQuestion();
+            csTrivia.generateChoices();
+        } else {
+            answerDiv.empty();
+            csTrivia.incorrectAnswers++;
+            csTrivia.generateQuestion();
+            csTrivia.generateChoices();
         }
     });
 
+$(document).ready(function() {
 
-
-$(document).ready({
-    
-}) 
+})
